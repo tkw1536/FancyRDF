f2eb8a22d9cddf610b7b0288a74a866319da2ef7
Tom Wiesing <tkw01536@gmail.com>
2026-02-15 19:28:50 +0100

    Term: remove internal tag

--- src/Term/Term.php
+++ src/Term/Term.php
@@ -13,8 +13,6 @@ use Override;
 /**
  * Represents a SPARQL Term.
  *
- * @internal
- *
  * @phpstan-import-type ResourceElement from Resource
  * @phpstan-import-type LiteralElement from Literal
  */
